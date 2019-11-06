408db3a854b4c032f55d9e5ba46bed21ec723d84
BobLiterman <jazz710@gmail.com>
2019-11-06 16:25:34 -0500

    Completed sameTopology

--- R/sameTopology.R
+++ R/sameTopology.R
@@ -1,8 +1,8 @@
-#' Check for Identical Tree Topology
+#' Rboretum Topology Checker
 #'
 #' This function returns TRUE if 'tree_1' and 'tree_2' have the same topology after pruning to identical species lists
-#' @param tree_1 Rooted phylo object
-#' @param tree_2 Rooted phylo object
+#' @param tree_1 Phylo object
+#' @param tree_2 Phylo object
 #' @return TRUE if 'tree_1' and 'tree_2' have the same topology after pruning to identical species lists; else, FALSE
 #' @export
 #' @examples
@@ -12,38 +12,27 @@
 # Returns TRUE if tree_1 and tree_2 have identical topology (after pruning if necessary)
 sameTopology <- function(tree_1,tree_2){
 
-  shared_species <- Rboretum::getSharedSpecies(c(tree_1,tree_))
-
   # If trees have identical species lists, compare topologies
-  if(identical(species_1,species_2)){
-
-    if(length(species_1)<3){
-      stop("Trees contain fewer than three species.")
-    }
-
+  
+  if(Rboretum::checkIdenticalSpecies(c(tree_1,tree_2))){
+    if(ape::all.equal.phylo(tree_1,tree_2,use.edge.length = FALSE)){
+      return(TRUE)}
     else{
-      if(ape::all.equal.phylo(tree_1,tree_2,use.edge.length = FALSE)){
-        return(TRUE)}
-      else{
-        return(FALSE)}
-    }
-  }
-
-  # If trees have different species lists, check >3 and prune
-  else{
-    if(has_error(Rboretum::checkSharedSpecies(tree_1,tree_2)) | !Rboretum::checkSharedSpecies(tree_1,tree_2)){
+      return(FALSE)}
+  } else{
+    
+    shared_species <- Rboretum::getSharedSpecies(c(tree_1,tree_2))
+    
+    if(length(shared_species) < 3){
       stop("Trees contain fewer than three shared species.")
-    } else{
-      shared_species <- Rboretum::getSharedSpecies(tree_1,tree_2)
-    }
-
-    pruned_tree_1 <- Rboretum::getTrimmedTree(tree_1,shared_species)
-    pruned_tree_2 <- Rboretum::getTrimmedTree(tree_2,shared_species)
-
-    if(ape::all.equal.phylo(pruned_tree_1,pruned_tree_2,use.edge.length = FALSE)){
-      return(TRUE)
-    } else{
-      return(FALSE)
-    }
+      } else{
+        
+        pruned_tree_1 <- Rboretum::getTrimmedTree(tree_1,shared_species)
+        pruned_tree_2 <- Rboretum::getTrimmedTree(tree_2,shared_species)
+        
+        if(ape::all.equal.phylo(pruned_tree_1,pruned_tree_2,use.edge.length = FALSE)){
+          return(TRUE)
+        } else{ return(FALSE) }
+      }
   }
 }

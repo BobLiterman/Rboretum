7ca184db97754faecb9e84a1e4f8541dfe000aa9
BobLiterman <jazz710@gmail.com>
2019-11-06 10:52:58 -0500

    Completed readRootedPhylo

--- R/readRootedPhylo.R
+++ R/readRootedPhylo.R
@@ -1,22 +1,23 @@
-#' Read Rooted Phylogenetic Tree from Path
+#' Rboretum Rooted Tree Reader
 #'
-#' This function returns a rooted tree, assuming the tree contains the outgroup taxa, and they are monophyletic.
-#' @param tree_path Path to tree file
-#' @param root_taxa Vector containing outgroup species IDs (Must be in tree and monophyletic)
-#' @return A phylo object rooted at specified taxa
+#' This function is an ape wrapper, and returns a tree rooted as specified by the user.
+#' @usage treePath <- '/path/to/my/tree.nwk'
+#' rootTaxa <- c('Root_species_1','Root_species_2')
+#' rootedTree <- readRootedPhylo(tree_path,root_taxa)
+#' @param tree_path Path to tree file that can be read by ape::read.tree()
+#' @param root_taxa Character vector containing outgroup species IDs (Must be in tree and monophyletic)
+#' @return A phylo object, rooted at specified taxa
 #' @export
 #' @examples
-#' readRootedPhylo('/path/to/tetrapod_tree.nwk',c('Fish_1','Fish_2'))
-#' OR
-#' tree_path <- '/path/to/tetrapod_tree.nwk'
-#' outgroup_species <- c('Fish_1','Fish_2')
-#' readRootedPhylo(tree_path,outgroup_species)
+#' myRootedTetrapods <- readRootedPhylo('/path/to/tetrapod_tree.nwk',c('Fish_1','Fish_2'))
 
 readRootedPhylo <- function(tree_path,root_taxa){
 
   # Read in tree and fetch species
-  raw_tree <- ape::read.tree(tree_path)
-  tree_species <- raw_tree$tip.label
+  if(!has_error(ape::read.tree(tree_path))){
+    raw_tree <- ape::read.tree(tree_path)
+    tree_species <- raw_tree$tip.label
+  } else{ stop("Path does not point to file that can be read in by ape::read.tree()")}
 
   # Ensure root species in tree, and are monophyletic for rooting
   if(!(all(root_taxa %in% tree_species))){

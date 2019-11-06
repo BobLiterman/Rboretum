5bf3c19cb9add401b68b82480622a63fa25719c6
BobLiterman <jazz710@gmail.com>
2019-11-06 17:16:16 -0500

    Completed getAllClades

--- R/getAllClades.R
+++ R/getAllClades.R
@@ -1,8 +1,8 @@
 #' Rboretum Clade Fetcher
 #'
-#' This function takes a tree and returns a vector containing each monophyletic group
+#' This function takes a tree and returns a sorted character vector containing each monophyletic group
 #' @param tree Phylo object
-#' @return Character vector of monophyletic clades
+#' @return Character vector of semicolon-separated monophyletic clades
 #' @export
 #' @examples
 #' getAllClades(tree)
@@ -10,6 +10,20 @@
 
 getAllClades <- function(tree){
   
+  if(!ape::is.rooted(tree)){
+    stop("Tree must be rooted for getAllClades")
+  }
+  
   splits <- Rboretum::getSplits(tree)
-
+  
+  root_split <- splits %>% filter(is.na(Split_Node))
+  
+  clades <- splits %>% 
+    filter(!is.na(Split_Node)) %>%
+    pull(Clade) %>%
+    as.character()
+  
+  clades <- c(clades,as.character(root_split$Clade),as.character(root_split$Mirror_Clade)) %>%
+    sort()
+  return(clades)
 }

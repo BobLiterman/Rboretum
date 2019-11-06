23921e5f2615f8638dc4c43c1e8e3d4b99b644c3
BobLiterman <jazz710@gmail.com>
2019-11-06 11:07:53 -0500

    Completed tableCount

--- R/tableCount.R
+++ R/tableCount.R
@@ -1,17 +1,17 @@
-#' Search Table for Value
+#' Rboretum Table Search
 #'
-#' This function returns the count of items in a table, or 0 if item does not occur in table
-#' @param table_to_search Table (Result of 'table' call)
-#' @param name_to_search Title of item you want the count for
+#' This function returns the count of items in an R table, or 0 if item does not occur in table
+#' @param search_table R Table (Result of 'table()' call)
+#' @param name Name of item you want the count for
 #' @return Number of times that value occurs in the table
 #' @export
 #' @examples
-#' tableCount(table_to_search,name_to_search)
+#' tableCount(table(geneDF$Count),'SOX9')
 #'
 
-tableCount <- function(table_to_search,name_to_search){
-  if(name_to_search %in% names(table_to_search)){
-    return(as.integer(table_to_search[name_to_search]))
+tableCount <- function(search_table,name){
+  if(name %in% names(search_table)){
+    return(as.integer(search_table[name]))
   }
   else{
     return(as.integer(0))

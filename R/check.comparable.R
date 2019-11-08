ad17d7895b7bbf530049ed108faa2ab22d932dc0
BobLiterman <jazz710@gmail.com>
2019-11-08 13:13:54 -0500

    Rename checkComparable to check.comparable

--- R/check.comparable.R
+++ R/check.comparable.R
@@ -1,18 +1,18 @@
 #' Rboretum Two-Tree Comparability Checker
 #'
 #' This function returns TRUE if 'tree_1' and 'tree_2' have 3 or more species in commmon, and a unique topology
-#' @param tree_1 Phylo object
-#' @param tree_2 Phylo object
+#' @param tree_1 phylo object
+#' @param tree_2 phylo object
 #' @return TRUE if 'tree_1' and 'tree_2' have 3 or more species in commmon and a unique topology; else, FALSE
 #' @export
 #' @examples
-#' checkComparable(tree_1,tree_2)
+#' check.comparable(tree_1,tree_2)
 #'
 
-checkComparable <- function(tree_1,tree_2){
-  if(!has_error(Rboretum::sameTopology(tree_1,tree_2))){
+check.comparable <- function(tree_1,tree_2){
+  if(!has_error(Rboretum::same.topology(tree_1,tree_2))){
     return(FALSE)
-  } else if(!Rboretum::sameTopology(tree_1,tree_2)){
+  } else if(!Rboretum::same.topology(tree_1,tree_2)){
     return(TRUE)
   } else{ return(FALSE) }
 }

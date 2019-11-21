f8ffd410594c06af43a891cd0fe7c9ea287c19e8
Rachel Schwartz <rsschwartz@gmail.com>
2019-11-21 07:20:58 -0500

    use map for readmulti

--- R/readMulti.rooted.R
+++ R/readMulti.rooted.R
@@ -7,26 +7,16 @@
 #' @export
 #' @examples
 #' myTrees <- c('/path/to/tree1','/path/to/tree2')
+#' myTrees <- list.files("/path/to/tree/folder/",full.names = TRUE)
 #' myRootTaxa <- c('Spp1','Spp2')
 #' myMulti <- readMulti.rooted(myTrees,myRootTaxa)
 
 readMulti.rooted <- function(tree_paths,root_taxa){
   if(length(tree_paths)<2){
     stop("'tree_paths' contains fewer than 2 items, use read.rooted()")
-  } else{ 
-    tree_count <- length(tree_paths)
-  }
+  } 
   
-  trees <- c(ape::rtree(10),ape::rtree(10))
-
-  for(i in 1:tree_count){
-    if(has_error(read.rooted(tree_paths[i],root_taxa))){
-      print(tree_paths[[i]])
-      stop("The above file above cannot be read or rooted using read.rooted()")
-    } else{
-      trees <- c(trees,read.rooted(tree_paths[i],root_taxa))
-    }
-  }
-  
-  trees <- trees[3:(2+tree_count)]
+  trees <- map(.x = tree_paths,root_taxa=root_taxa, .f = read.rooted)
+  class(trees) <- "multiPhylo"
+  return(trees)
 }

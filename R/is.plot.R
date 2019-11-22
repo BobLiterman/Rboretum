f39663c2eb2d47ce24699df3c0172d301f41f2f6
BobLiterman <jazz710@gmail.com>
2019-11-22 05:58:43 -0500

    Added is.plot

---
+++ R/is.plot.R
@@ -0,0 +1,20 @@
+#' Rboretum Plot Checker
+#'
+#' This function returns TRUE if the passed object is of class ggtree or ggplot; Otherwise, FALSE
+#' @param test_object R object to check
+#' @return TRUE if plot, otherwise FALSE
+#' @export
+#'
+is.plot <- function(test_object){
+  if(has_error(unlist(attributes(test_object)))){
+    return(FALSE)
+  } else{
+    test_object_class <- unlist(attributes(test_object)$class)
+
+    if(!any(c('ggtree','ggplot') %in% test_object_class)){
+      return(FALSE)
+    } else{
+      return(TRUE)
+    }
+  }
+}

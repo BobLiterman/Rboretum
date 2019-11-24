2232eb6239c859920a92b9240b856602963c2803
BobLiterman <jazz710@gmail.com>
2019-11-24 09:04:43 -0500

    Allow defaults  for convert.tips

--- R/convert.tips.R
+++ R/convert.tips.R
@@ -3,8 +3,8 @@
 #' This function takes a tree and changes tip labels based on a supplied dataframe
 #' @param tree phylo object
 #' @param name_df Dataframe with name equivalencies
-#' @param from Column name with current tree tip IDs
-#' @param to Column name with desired IDs
+#' @param from Column name with current tree tip IDs (if missing, first column of name_df used as default)
+#' @param to Column name with desired IDs (if missing, second column of name_df used as default)
 #' @return phylo object with converted names
 #' @export
 #' @examples
@@ -13,21 +13,27 @@
 
 convert.tips <- function(tree,name_df,from,to){
   
-  if(!Rboretum::is.phylo(tree)){
+  if(missing(tree)){
+    stop("No 'tree' provided")
+  } else if(!Rboretum::is.phylo(tree)){
     stop("'tree' does not appear to be a valid phylo object.")
   } else{ tree_taxa <- tree$tip.label }
   
   if(missing(name_df)){
     stop("No name table provided to name_df")
+  } else if(ncol(name_df)<2){
+    stop("'name_df' must at least have two columns [current IDs, new IDs]")
   }
   
   if(missing(from)){
-    stop("Must specify column header with current tree IDs with 'from' ")
+    print("No 'from' column name provided. Defaulting to column 1 of 'name_df'...")
+    from <- names(name_df)[1]
   } else{ from <- as.character(from) }
   
   if(missing(to)){
-    stop("Must specify column header with desired tree IDs with 'to' ")
-  } else{ from <- as.character(to) }  
+    print("No 'to' column name provided. Defaulting to column 2 of 'name_df'...")
+    to <- names(name_df)[2]
+  } else{ to <- as.character(to) } 
   
   if(!all(c(to,from) %in% names(name_df))){
     stop("Either 'to' or 'from' column not found in 'name_df'")
@@ -38,9 +44,7 @@ convert.tips <- function(tree,name_df,from,to){
 
   if(!all(tree_taxa %in% current_ids)){
     stop("Specified 'from' column does not contain all IDs from tree")
-  }
-
-  if(any(duplicated(current_ids))){
+  } else if(any(duplicated(current_ids))){
     stop("Specified 'from' column contains duplicate IDs")
   }
 

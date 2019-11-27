4903413d67c0cfc26faf873782c8e4af0fa9e3e5
BobLiterman <jazz710@gmail.com>
2019-11-27 12:14:58 -0500

    change scale_color_viridis to scale_color_manual with viridis PAL

--- R/supportTreePlotter.R
+++ R/supportTreePlotter.R
@@ -419,7 +419,7 @@ supportTreePlotter <- function(tree,tree_support,clade_support,support_scales,no
     return_tree <- return_tree + 
       geom_nodepoint(alpha=node_alpha,aes(size=scaled_support,color=as.integer(tree_count))) + 
       scale_size_identity() +
-      scale_color_viridis(breaks = 1:max(ggtree_df$tree_count),name = "Trees with Split") +
+      scale_color_manual(breaks = 1:max(ggtree_df$tree_count),values = viridisLite::viridis(length(1:max(ggtree_df$tree_count))),name = "Trees with Split",drop = FALSE) +
       theme(legend.position="right",
             legend.title=element_text(size=legend_title_size), 
             legend.text=element_text(size=legend_font_size)) +

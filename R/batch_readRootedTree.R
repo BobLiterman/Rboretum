a8594d5633cd98626d7b405614ebf32b4dbe75b0
BobLiterman <jazz710@gmail.com>
2019-11-27 10:34:15 -0500

    Allow tree naming in batch_readRootedTree

--- R/batch_readRootedTree.R
+++ R/batch_readRootedTree.R
@@ -3,6 +3,7 @@
 #' This function is an ape wrapper, and returns a multiPhylo object, with each tree rooted as specified by the user.
 #' @param tree_paths Vector of paths to tree files that can be read by ape::read.tree() or ape::read.nexus()
 #' @param root_taxa Character vector containing outgroup species IDs (Must be in all trees and always monophyletic)
+#' @param tree_names OPTIONAL: Character vector of names to assign to trees. Length must equal the number of trees.
 #' @return A multiPhylo object, with each tree rooted at specified taxa
 #' @export
 #' @examples
@@ -10,7 +11,7 @@
 #' myRootTaxa <- c('Spp1','Spp2')
 #' myMulti <- readMulti.rooted(myTrees,myRootTaxa)
 
-batch_readRootedTree <- function(tree_paths,root_taxa){
+batch_readRootedTree <- function(tree_paths,root_taxa,tree_names){
   
   if(length(tree_paths)<2){
     stop("'tree_paths' contains fewer than 2 items, use readRootedTree()")
@@ -18,5 +19,14 @@ batch_readRootedTree <- function(tree_paths,root_taxa){
   
   trees <- purrr::map(.x = tree_paths,root_taxa=root_taxa, .f = readRootedTree)
   class(trees) <- "multiPhylo"
-  return(trees)
+  
+  if(missing(tree_names)){
+    return(trees)
+  } else if(length(tree_names) != length(trees)){
+    print("Incorrect number of tree names provided. Returning unnamed trees.")
+    return(trees)
+  } else{
+    names(trees) <- tree_names
+    return(trees)
+  }
 }

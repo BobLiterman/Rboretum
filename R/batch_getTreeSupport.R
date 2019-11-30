b0ae04def659609c2a329e5aae8e0b08e1fe3c41
BobLiterman <jazz710@gmail.com>
2019-11-30 11:07:09 -0500

    Created isTreeSupport, isAlignmentSignal, started batch functions

--- R/batch_getTreeSupport.R
+++ R/batch_getTreeSupport.R
@@ -1,8 +1,17 @@
-#' Rboretum Alignment Signal Support Mapper
+#' Rboretum Batch Alignment Signal Support Mapper
 #'
 #' This function computes the alignment support for a specified tree, processing signal from all alignment columns with <= 'max_missing' missing taxa [from output of getAlignmentSignal()]. Signal from multiple alignments/missing combinations can be added to the same table by passing the output of tree.support() to the 'existing_splits' argument
-#' @param signal Output table from getAlignmentSignal()
-#' @param tree phylo object where taxa EXACTLY match those from signal
+#' @param signal
+#' \itemize{
+#'   \item Output table from getAlignmentSignal(); or, 
+#'   \item Output table from batch_getAlignmentSignal(); or, 
+#'   \item A list of individual outputs
+#' }
+#' @param tree 
+#' \itemize{
+#'   \item phylo object where taxa EXACTLY match those from signal; or, 
+#'   \item multiPhylo object where all trees have identical taxa that EXACTLY match those from signal
+#' }
 #' @param max_missing OPTIONAL: Number of missing sites allowed in alignment column [Default: 0]
 #' @param alignment_name OPTIONAL: Column name for data being added [Default: Alignment name from signal dataframe]
 #' @param include_gap OPTIONAL: TRUE or FALSE; Count sites with gap positions ('-') as part of total support [Default: TRUE]
@@ -16,7 +25,7 @@
 #' @return The same split table from getTreeSplits(tree), but with a support column for the specfied alignment/missing combination
 #' @export
 
-getTreeSupport <- function(signal,tree,max_missing,alignment_name,include_gap,include_singleton,include_biallelic,include_triallelic,include_quadallelic,include_pentallelic,only_gap,existing_splits){
+batch_getTreeSupport <- function(signal,tree,max_missing,alignment_name,include_gap,include_singleton,include_biallelic,include_triallelic,include_quadallelic,include_pentallelic,only_gap,existing_splits){
 
   if(!all(names(signal) == c('Alignment_Name','Alignment_Position','Site_Pattern','Gap','Singleton','Singleton_Taxa','Non_Base_Taxa','Non_Base_Count','Split_1','Split_2','Split_3','Split_4','Split_5'))){
     stop("'signal' argument must be output from getAlignmentSignal()")
---
+++ R/isAlignmentSignal.R
@@ -0,0 +1,50 @@
+#' Rboretum Alignment Signal Checker
+#'
+#' This function returns TRUE if the passed object is the output of getAlignmentSignal; otherwise, FALSE
+#' @param test_object R object to check
+#' @param species_info OPTIONAL: Check that passed signal contains information about a specific set of taxa. Set of 3+ species that can be passed as either:
+#' \itemize{
+#'   \item phylo object from which species will be extracted; or
+#'   \item Character vector of desired taxa
+#' }
+#' @return TRUE if output of getAlignmentSignal(); otherwise, FALSE
+#' @export
+
+isAlignmentSignal <- function(test_object,species_info){
+  
+  if(!is.data.frame(test_object)){
+    return(FALSE)
+  } else if(nrow(test_object) == 0){
+    return(FALSE)
+  } else if(has_error(all(names(test_object) == c('Alignment_Name','Alignment_Position','Site_Pattern','Gap','Singleton','Singleton_Taxa','Non_Base_Taxa','Non_Base_Count','Split_1','Split_2','Split_3','Split_4','Split_5')),silent = TRUE)){
+    return(FALSE)
+  } else if(!all(names(test_object) == c('Alignment_Name','Alignment_Position','Site_Pattern','Gap','Singleton','Singleton_Taxa','Non_Base_Taxa','Non_Base_Count','Split_1','Split_2','Split_3','Split_4','Split_5'))){
+    return(FALSE)
+  }
+  
+  if(missing(species_info)){
+    return(TRUE)
+  } else{
+    
+    signal_taxa <- test_object %>%
+      filter(!is.na(Split_1)) %>% 
+      head(1) %>% 
+      select(Singleton_Taxa,Non_Base_Taxa,Split_1,Split_2,Split_3,Split_4,Split_5) %>% 
+      select_if(~ !any(is.na(.))) %>% 
+      unite(col = "Taxa",sep = ";") %>% 
+      semiVector() %>% 
+      sort()
+    
+    if(Rboretum::isPhylo(species_info)){
+      spp_list = sort(species_info$tip.label)
+    } else if(typeof(species_info)=="character" && length(species_info) > 3){
+      spp_list = sort(species_info)
+    } else { stop("'species_info' is not a phylo object or character vector 3+ species IDs") }  
+    
+    if(all(signal_taxa == species_info)){
+      return(TRUE)
+    } else{
+      return(FALSE)
+    }
+  }
+}

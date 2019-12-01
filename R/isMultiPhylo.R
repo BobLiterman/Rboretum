c7f89c1d6d8f1baa6067447a7df0a12190e4fc25
BobLiterman <jazz710@gmail.com>
2019-12-01 07:35:44 -0500

    Add check_rooted to to isPhylo/isMultiPhylo

--- R/isMultiPhylo.R
+++ R/isMultiPhylo.R
@@ -2,17 +2,37 @@
 #'
 #' This function returns TRUE if the passed object is of class multiPhylo and has 2+ trees; Otherwise, FALSE
 #' @param test_object R object to check
+#' @param check_rooted OPTIONAL: If TRUE, also check if tree is rooted. Default: FALSE, don't check rootedness
 #' @return TRUE if multiPhylo with 2+ trees, otherwise FALSE
 #' @export
-#'
-isMultiPhylo <- function(test_object){
+
+isMultiPhylo <- function(test_object,check_rooted){
+  
+  if(missing(check_rooted)){
+    check_rooted <- FALSE
+  } else if(!is.logical(check_rooted)){
+    check_rooted <- FALSE
+  }
+  
   if(has_error(silent=TRUE,expr=unlist(attributes(test_object)))){
     return(FALSE)
   } else{
     test_object_class <- unlist(attributes(test_object)$class)
 
     if('multiPhylo' %in% test_object_class & length(test_object)>=2){
-      return(TRUE)
+      if(!check_rooted){
+        return(TRUE)
+      } else{
+        root_check  <- c()
+        for(i in 1:length(test_object)){
+          root_check <- c(root_check,ape::is.rooted(test_object[[i]]))
+        }
+        if(all(root_check)){
+          return(TRUE)
+        } else{
+          return(FALSE)
+        }
+      }
     } else{
       return(FALSE)
     }
--- R/isPhylo.R
+++ R/isPhylo.R
@@ -2,17 +2,33 @@
 #'
 #' This function returns TRUE if the passed object is of class phylo; Otherwise, FALSE
 #' @param test_object R object to check
+#' @param check_rooted OPTIONAL: If TRUE, also check if tree is rooted. Default: FALSE, don't check rootedness
 #' @return TRUE if phylo, otherwise FALSE
 #' @export
-#'
-isPhylo <- function(test_object){
+
+isPhylo <- function(test_object,check_rooted){
+  
+  if(missing(check_rooted)){
+    check_rooted <- FALSE
+  } else if(!is.logical(check_rooted)){
+    check_rooted <- FALSE
+  }
+  
   if(has_error(silent=TRUE,expr=unlist(attributes(test_object)))){
     return(FALSE)
   } else{
     test_object_class <- unlist(attributes(test_object)$class)
 
     if('phylo' %in% test_object_class){
-      return(TRUE)
+      if(!check_rooted){
+        return(TRUE)
+      } else{
+        if(ape::is.rooted(test_object)){
+          return(TRUE)
+        } else{
+          return(FALSE)
+        }
+      }
     } else{
       return(FALSE)
     }

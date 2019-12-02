8d75f4cb04e5b1d272fb875576e0bcb5ab3ef667
BobLiterman <jazz710@gmail.com>
2019-12-02 16:00:13 -0500

    Add check_names to isMultiphylo

--- R/isMultiPhylo.R
+++ R/isMultiPhylo.R
@@ -2,16 +2,23 @@
 #'
 #' This function returns TRUE if the passed object is of class multiPhylo and has 2+ trees; Otherwise, FALSE
 #' @param test_object R object to check
-#' @param check_rooted OPTIONAL: If TRUE, also check if tree is rooted. Default: FALSE, don't check rootedness
-#' @return TRUE if multiPhylo with 2+ trees, otherwise FALSE
+#' @param check_rooted OPTIONAL: If TRUE, also check if all trees are rooted. Default: FALSE, don't check rootedness
+#' @param check_named OPTIONAL: If TRUE, also check if trees are named. Default: FALSE, don't check names
+#' @return TRUE if all flagged conditions match, otherwise FALSE
 #' @export
 
-isMultiPhylo <- function(test_object,check_rooted){
+isMultiPhylo <- function(test_object,check_rooted,check_names){
   
   if(missing(check_rooted)){
     check_rooted <- FALSE
   } else if(!is.logical(check_rooted)){
     check_rooted <- FALSE
+  }
+  
+  if(missing(check_names)){
+    check_names <- FALSE
+  } else if(!is.logical(check_names)){
+    check_names <- FALSE
   }
   
   if(has_error(silent=TRUE,expr=unlist(attributes(test_object)))){
@@ -20,21 +27,35 @@ isMultiPhylo <- function(test_object,check_rooted){
     test_object_class <- unlist(attributes(test_object)$class)
 
     if('multiPhylo' %in% test_object_class & length(test_object)>=2){
-      if(!check_rooted){
-        return(TRUE)
-      } else{
-        root_check  <- c()
-        for(i in 1:length(test_object)){
-          root_check <- c(root_check,ape::is.rooted(test_object[[i]]))
+      
+      root_check  <- c()
+      for(i in 1:length(test_object)){
+        root_check <- c(root_check,ape::is.rooted(test_object[[i]]))
+      }
+      name_check <- is.null(names(test_object))
+      
+      if(check_names & check_rooted){
+        if(all(root_check) & !name_check){
+          return(TRUE)
+        } else{
+          return(FALSE)
         }
+      } else if(check_names & !check_rooted){
+        if(!name_check){
+          return(TRUE)
+        } else{
+          return(FALSE)
+        }
+      } else if(!check_names & check_rooted){
         if(all(root_check)){
           return(TRUE)
         } else{
           return(FALSE)
         }
+      } else if(!check_names & !check_rooted){
+        return(TRUE)
       }
-    } else{
-      return(FALSE)
     }
+    
   }
 }

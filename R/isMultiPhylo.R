bfebe22550a7460dfab766807108d7dfadc81006
BobLiterman <jazz710@gmail.com>
2019-12-04 16:26:38 -0500

    Update isMultPhylo to rely on no other functions

--- R/isMultiPhylo.R
+++ R/isMultiPhylo.R
@@ -2,15 +2,27 @@
 #'
 #' This function returns TRUE if the passed object is of class multiPhylo and has 2+ trees; Otherwise, FALSE
 #' @param test_object R object to check
-#' @param check_rooted OPTIONAL: If TRUE, also check if all trees are rooted. Default: FALSE, don't check rootedness
-#' @param check_named OPTIONAL: If TRUE, also check if trees are named. Default: FALSE, don't check names
-#' @param check_three_taxa OPTIONAL: If TRUE, also check if all trees share at least three taxa. Default: FALSE, don't check taxa
-#' @param check_all_taxa OPTIONAL: If TRUE, also check if trees share identical taxa. Default: FALSE, don't check taxa
-#' @param check_unique OPTIONAL: If TRUE, also check if trees all have a unique topology. Default: FALSE, don't check topology
-#' @return TRUE if all flagged conditions match, otherwise FALSE
+#' @param check_named OPTIONAL: If TRUE, also check if trees are named. [Default: FALSE, don't check names]
+#' @param check_rooted OPTIONAL: If TRUE, also check if all trees are rooted. [Default: FALSE, don't check rootedness]
+#' @param check_three_taxa OPTIONAL: If TRUE, also check if all trees share at least three taxa. [Default: FALSE, don't check taxa]
+#' @param check_all_taxa OPTIONAL: If TRUE, also check if all trees share all taxa. [Default: FALSE, don't check taxa]
+#' @param verbose OPTIONAL: If TRUE: Print messages along the way; else execute silently. [Default: FALSE]
+#' @return TRUE if 'test_object' is a multiPhylo with 2+ trees; otherwise, FALSE
 #' @export
 
-isMultiPhylo <- function(test_object,check_rooted,check_names,check_three_taxa,check_all_taxa,check_unique){
+isMultiPhylo <- function(test_object,check_named,check_rooted,check_three_taxa,check_all_taxa,verbose){
+  
+  if(missing(verbose)){
+    verbose <- FALSE
+  } else if(!is.logical(verbose)){
+    verbose <- FALSE
+  }
+  
+  if(missing(check_named)){
+    check_named <- FALSE
+  } else if(!is.logical(check_named)){
+    check_named <- FALSE
+  }
   
   if(missing(check_rooted)){
     check_rooted <- FALSE
@@ -18,87 +30,45 @@ isMultiPhylo <- function(test_object,check_rooted,check_names,check_three_taxa,c
     check_rooted <- FALSE
   }
   
-  if(missing(check_names)){
-    check_names <- FALSE
-  } else if(!is.logical(check_names)){
-    check_names <- FALSE
-  }
-
   if(missing(check_three_taxa)){
     check_three_taxa <- FALSE
   } else if(!is.logical(check_three_taxa)){
     check_three_taxa <- FALSE
   }
-    
+  
   if(missing(check_all_taxa)){
     check_all_taxa <- FALSE
   } else if(!is.logical(check_all_taxa)){
     check_all_taxa <- FALSE
   }
   
-  if(missing(check_unique)){
-    check_unique <- FALSE
-  } else if(!is.logical(check_unique)){
-    check_unique <- FALSE
-  }
-  
   if(has_error(silent=TRUE,expr=unlist(attributes(test_object)))){
+    if(verbose){ print("Can't execute unlist(attributes(test_object))")}
     return(FALSE)
   } else{
-    test_object_class <- unlist(attributes(test_object)$class)
-
-    if('multiPhylo' %in% test_object_class & length(test_object)>=2){
-      
-      if(!check_rooted){
-        root_check <- TRUE
-      } else{
-      root_check  <- c()
-      for(i in 1:length(test_object)){
-        root_check <- c(root_check,ape::is.rooted(test_object[[i]]))
-      }
-      root_check <- all(root_check)
-      }
-      
-      if(!check_names){
-        name_check <- TRUE
-      } else{
-        name_check <- !is.null(names(test_object))
-      }
-      
-      if(!check_three_taxa){
-        three_taxa_check <- TRUE
-      } else{
-        three_taxa_check <- Rboretum::checkSharedTaxa(test_object)
-      }
-      
-      if(!check_all_taxa){
-        all_taxa_check <- TRUE
-      } else{
-        all_taxa_check <- Rboretum::checkSameTaxa(test_object)
-      }
-      
-      if(!check_unique){
-        unique_check  <- TRUE
-      } else if(has_error(silent = TRUE,Rboretum::compareTrees(test_object))){
-        unique_check <- FALSE
-      } else{
-        
-        compare_vector <- Rboretum::compareTrees(test_object) %>% pull(Comparable)
-      
-        if(all(compare_vector)){
-          unique_check <- TRUE
-        } else{
-          unique_check <- FALSE
-        }
-      }
     
-      if(all(c(root_check,name_check,three_taxa_check,all_taxa_check,unique_check))){
-        return(TRUE)
-      } else{
-        return(FALSE)
-      }
-    } else{
+    test_object_class <- unlist(attributes(test_object)$class)
+    
+    if(!'multiPhylo' %in% test_object_class){
+      if(verbose){ print("'multiPhylo' not an attribute of test_object")}
       return(FALSE)
+    } else if(length(test_object) < 2){
+      if(verbose){ print("'multiPhylo' contains fewer than two trees.")}
+      return(FALSE)
+    } else if(check_named & is.null(names(test_object))){
+      if(verbose){ print("'test_object' has no names assigned.")}
+      return(FALSE)
+    } else if(check_rooted & !all(unlist(purrr::map(.x = test_object,.f = ape::is.rooted)))){
+      if(verbose){ print("One or more trees from 'test_object' are not rooted.")}
+      return(FALSE)
+    } else if(check_three_taxa & !purrr::map(.x = test_object,.f = function(x){return(x$tip.label)}) %>% unlist() %>% table() %>% (function(x){x==length(test_object)}) %>% sum() >= 3){
+      if(verbose){ print("Trees share fewer than three common taxa.")}
+      return(FALSE)
+    } else if(check_all_taxa & !purrr::map(.x = test_object,.f = function(x){return(x$tip.label)}) %>% unlist() %>% table() %>% (function(x){x==length(test_object)}) %>% sum() == purrr::map(.x = test_object,.f = function(x){return(x$tip.label)}) %>% unlist() %>% unique() %>% length()){
+      if(verbose){ print("Trees do not share all taxa.")}
+      return(FALSE)
+    } else{
+      return(TRUE)
     }
   }
 }

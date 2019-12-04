5fe104a2ea449d2891311b810aa62516915a46a8
BobLiterman <jazz710@gmail.com>
2019-12-04 18:31:06 -0500

    Updated getSharedTaxa

--- R/getSharedTaxa.R
+++ R/getSharedTaxa.R
@@ -9,11 +9,13 @@ getSharedTaxa <- function(trees){
   
   # Check that input is multiphylo and has at least 2 trees
   if(!Rboretum::isMultiPhylo(trees, check_three_taxa = TRUE)){
-    stop("'trees' does not appear to be a valid multiPhylo object with 2+ trees")
+    stop("'trees' does not appear to be a valid multiPhylo where all trees share at least three taxa.")
   }
 
   all_species <- purrr::map(.x=trees,.f=function(x){x$tip.label}) %>% unlist() %>% unique() %>% sort()
-  shared_species <- all_species[table(purrr::map(.x=trees,.f=function(x){x$tip.label}) %>% unlist())==length(trees)]
+  tip_table  <- purrr::map(.x=trees,.f=function(x){x$tip.label}) %>% unlist() %>% table()
+  
+  shared_species <- all_species[tip_table==length(trees)]
   
   return(shared_species)
 }

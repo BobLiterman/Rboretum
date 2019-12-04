1713782bda3dd9f1194cc38dea9e30d464ac65ec
BobLiterman <jazz710@gmail.com>
2019-12-04 17:56:47 -0500

    Updated getSharedTaxa

--- R/getSharedTaxa.R
+++ R/getSharedTaxa.R
@@ -1,24 +1,19 @@
 #' Rboretum Common Taxa Fetcher
 #'
-#' This function takes a multiPhylo object and returns a sorted list of taxa common to all trees. Note: Function will STOP if no taxa are shared.
-#' @param trees multiPhylo object
+#' This function takes a multiPhylo object and returns a sorted list of taxa common to all trees. Note: Function will STOP if <3 taxa are shared among trees.
+#' @param trees multiPhylo object where >= 3 species are shared among all trees
 #' @return Sorted character vector of tip labels present in all trees
 #' @export
 
 getSharedTaxa <- function(trees){
   
   # Check that input is multiphylo and has at least 2 trees
-  if(!Rboretum::isMultiPhylo(trees)){
+  if(!Rboretum::isMultiPhylo(trees, check_three_taxa = TRUE)){
     stop("'trees' does not appear to be a valid multiPhylo object with 2+ trees")
   }
-  
-  tree_species <- c()
-  for(i in 1:length(trees)){
-    tree_species <- c(tree_species,trees[[i]]$tip.label)
-  }
-  
-  all_species <- sort(unique(tree_species))
-  shared_species <- all_species[table(tree_species)==length(trees)]
+
+  all_species <- purrr::map(.x=trees,.f=function(x){x$tip.label}) %>% unlist() %>% unique() %>% sort()
+  shared_species <- all_species[table(purrr::map(.x=trees,.f=function(x){x$tip.label}) %>% unlist())==length(trees)]
   
   if(length(shared_species)>0){
     return(shared_species)

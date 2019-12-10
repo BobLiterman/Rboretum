38f0476fc662d2e209b464a54f167925161333cd
BobLiterman <jazz710@gmail.com>
2019-12-10 16:55:49 -0500

    Added getTreeSupport

---
+++ R/getTreeSupport.R
@@ -0,0 +1,182 @@
+#' Rboretum Alignment Signal Support Mapper
+#'
+#' This function maps phylogenetic signal from a multiple-sequence alingment onto a rooted phylo or multiPhylo object
+#' @param tree Tree(s) onto which alignment signal will be mapped. Options include:
+#' \itemize{
+#'   \item A rooted phylo object
+#'   \item A rooted, named multiPhylo object where all trees share 3+ taxa
+#' }
+#' @param signal Output table from getAlignmentSignal() run with taxa against 'tree'
+#' @param alignment_name OPTIONAL: Column name(s) for data being added [Default: Alignment name from signal dataframe + 'm_<MISSING>']
+#' @param max_missing OPTIONAL: Number of missing sites allowed in alignment column before it is not considered [Default: 0, no missing taxa allowed]
+#' @param include_gap OPTIONAL: If TRUE, count sites with gap positions ('-') as informative signal; otherwise, count gaps as missing data [Default: FALSE: Gaps are treated as missing]
+#' @param only_gap OPTIONAL: TRUE or FALSE; Only count sites with gap positions ('-') [Default: FALSE]
+#' @param include_singleton OPTIONAL: If TRUE, count sites with singletons as part of total support [Default: TRUE]
+#' @param include_biallelic OPTIONAL: If TRUE, count sites with biiallelic variation as part of total support [Default: TRUE]
+#' @param include_triallelic OPTIONAL: If TRUE, count sites with triallelic variation as part of total support [Default: TRUE]
+#' @param include_quadallelic OPTIONAL: If TRUE, count sites with quadallelic variation as part of total support [Default: TRUE]
+#' @param include_pentallelic OPTIONAL: If TRUE, count sites with pentallelic variation as part of total support [Default: TRUE]
+#' @param existing_support OPTIONAL: Append these results to the output from getTreeSupport() run with the same 'tree' and different alignment options
+#' @return The same split table from getTreeSplits(tree), but with (1) no root row, and (2) a support column for the specfied alignment/missing combination
+#' @export
+
+getTreeSupport <- function(tree,signal,alignment_name,max_missing,include_gap,only_gap,include_singleton,include_biallelic,include_triallelic,include_quadallelic,include_pentallelic){
+
+  # Check if tree and signal are valid and compatible
+  if(!Rboretum::isPhylo(tree,check_rooted = TRUE) & !Rboretum::isMultiPhylo(tree,check_named = TRUE,check_rooted = TRUE, check_three_taxa = TRUE)){
+    stop("'tree' must be a rooted phylo object, or a named, rooted multiPhylo for getTreeSupport")
+  } else if(!Rboretum::isAlignmentSignal(signal,tree)){
+    stop("'signal' is either not the output from getAlignmentSignal(), or does not contain the same taxa as 'tree'")
+  } else if(Rboretum::isPhylo(tree)){
+    tree_count <- 1
+    tree_taxa <- sort(tree$tip.label) # Get tree taxa
+  } else{ # If tree is a multiPhylo
+    
+    tree_taxa <- Rboretum::getSharedTaxa(tree)
+    
+    # Reduce 'tree' to unique topologies
+    if(Rboretum::isMultiPhylo(tree,check_all_equal=TRUE)){
+      tree_count <- 1
+      tree <- as.phylo(tree[[1]])
+    } else if(Rboretum::isMultiPhylo(tree,check_all_unique = TRUE)){
+      tree_count <- length(tree)
+      tree_names <- names(trees)
+    } else if(Rboretum::isMultiPhylo(tree,check_some_equal = TRUE)){
+      tree <- Rboretum::getUniqueTopologies(tree)
+      tree_count <- length(tree)
+      tree_names <- names(trees)
+    }
+  }
+  
+  # Set maximum number of missing taxa allowed
+  max_possible_missing <- length(tree_taxa) - 3
+  
+  if(missing(max_missing)){
+    max_missing <- 0
+  } else if(!is.integer(max_missing)){
+    stop("'max_missing' must be an integer")
+  } else{
+    if(max_missing > max_possible_missing){
+      max_missing <- max_possible_missing
+    }
+  }
+  
+  if(missing(include_gap)){
+    include_gap <- FALSE
+  } else if (!is.logical(include_gap)){
+    include_gap <- FALSE
+  }
+  
+  if(missing(only_gap)){
+    only_gap <- FALSE
+  } else if (!is.logical(only_gap)){
+    only_gap <- FALSE
+  }
+  
+  if(missing(include_singleton)){
+    include_singleton <- TRUE
+  } else if (!is.logical(include_singleton)){
+    include_singleton <- TRUE
+  }
+  
+  if(missing(include_biallelic)){
+    include_biallelic <- TRUE
+  } else if (!is.logical(include_biallelic)){
+    include_biallelic <- TRUE
+  }
+  
+  if(missing(include_triallelic)){
+    include_triallelic <- TRUE
+  } else if (!is.logical(include_triallelic)){
+    include_triallelic <- TRUE
+  }
+  
+  if(missing(include_quadallelic)){
+    include_quadallelic <- TRUE
+  } else if (!is.logical(include_quadallelic)){
+    include_quadallelic <- TRUE
+  }
+  
+  if(missing(include_pentallelic)){
+    include_pentallelic <- TRUE
+  } else if (!is.logical(include_pentallelic)){
+    include_pentallelic <- TRUE
+  }
+  
+  if(missing(existing_support)){
+    add_support <- FALSE
+  } else if(!Rboretum::isTreeSupport(existing_support,tree)){
+    stop("'existing_support' is either not the output from getTreeSupport(), or does not contain split information from 'tree'")
+  } else{
+    add_support <- TRUE
+  }
+
+  informative_patterns <- c('biallelic','triallelic','quadallelic','pentallelic')
+  
+  signal <- signal %>% 
+    filter(Non_Base_Count <= max_missing) %>%
+    filter(Site_Pattern %in% informative_patterns)
+  
+  if(only_gap){
+    signal <- signal %>%
+      filter(Gap==TRUE)
+  }
+  
+  if(!include_singleton){
+    signal <- signal %>%
+      filter(Singleton==FALSE)
+  }
+  
+  if(!include_biallelic){
+    signal <- signal %>%
+      filter(!str_detect(Site_Pattern,'biallelic'))
+  }
+  
+  if(!include_triallelic){
+    signal <- signal %>%
+      filter(!str_detect(Site_Pattern,'triallelic'))
+  }
+  
+  if(!include_quadallelic){
+    signal <- signal %>%
+      filter(!str_detect(Site_Pattern,'quadallelic'))
+  }
+  
+  if(!include_pentallelic){
+    signal <- signal %>%
+      filter(!str_detect(Site_Pattern,'pentallelic'))
+  }
+  
+  # Figure out alignment count after filtering
+  if(nrow(signal)==0){
+    stop("No data fits the filtering criteria.")
+  }
+  
+  raw_alignment_name <- unique(signal$Alignment_Name)
+  alignment_count <- length(raw_alignement_name)
+
+  if(alignment_count == 1){
+    default_name <- paste(c(raw_alignment_name,"_m",max_missing),collapse = '')
+  } else{
+    default_name <- purrr::map(.x = raw_alignment_name,.f = function(x){paste(c(x,"_m",max_missing),collapse = '')}) %>% unlist() %>% as.character()
+  }
+  
+  # Set alignment names to defaults if necessary
+  if(missing(alignment_name)){
+    alignment_name <- default_name
+  } else if(!is.character(alignment_name)){
+    alignment_name <- default_name
+  } else if(length(alignment_name) != alignment_count){
+    alignment_name <- default_name
+  }
+
+  if(tree_count == 1){
+    support_df <- Rboretum::getTreeSupport_Worker(tree,signal,alignment_name)
+    return(support_df)
+  } else{
+    support_list <- purrr::map(.x=tree,.f = function(x){Rboretum::getTreeSupportWorker(x,signal,alignment_name)})
+    names(support_list) <- tree_names
+    return(support_list)
+  }
+
+}
---
+++ R/isTreeSupport.R
@@ -0,0 +1,111 @@
+#' Rboretum Alignment Signal Checker
+#'
+#' This function returns TRUE if the passed object is the output of getAlignmentSignal; otherwise, FALSE
+#' @param test_object R object to check
+#' @param tree OPTIONAL: Check that passed support object contains information about a specific set of trees. Options include:
+#' \itemize{
+#'   \item A rooted phylo object; or,
+#'   \item A named, rooted multiPhylo object where all trees share 3+ taxa
+#' }
+#' @return TRUE if output of getTreeSupport(); otherwise, FALSE
+#' @export
+
+isTreeSupport <- function(test_object,tree){
+  
+  # Ensure dataframe columns match expected and that data exists
+  if(!is.data.frame(test_object) & !is.list(test_object)){
+    return(FALSE)
+  } else if(!Rboretum::isPhylo(tree,check_rooted = TRUE) & !Rboretum::isMultiPhylo(tree,check_rooted = TRUE,check_named = TRUE,check_three_taxa = TRUE)){
+    stop("'tree' is not a valid rooted phylo or named, rooted multiPhylo object")
+  }
+  
+  # Get tree clades to test
+  if(Rboretum::isPhylo(tree)){
+    tree_clades <- Rboretum::getTreeClades(tree)
+    tree_count <- 1
+  } else if(Rboretum::isMultiPhylo(tree,check_all_equal = TRUE)){
+    tree <- as.phylo(tree[[1]])
+    tree_clades <- Rboretum::getTreeClades(tree)
+    tree_count <- 1
+  } else if(Rboretum::isMultiPhylo(tree,check_all_unique = TRUE)){
+    tree_names <- names(tree)
+    tree_clades <- purrr::map(.x=tree,.f=function(x){Rboretum::getTreeClades(x)})
+    names(tree_clades) <- tree_names
+    tree_count <- length(tree)
+  } else if(Rboretum::isMultiPhylo(tree,check_some_unique = TRUE)){
+    tree <- Rboretum::getUniqueTopologies(tree)
+    tree_names <- names(tree)
+    tree_clades <- purrr::map(.x=tree,.f=function(x){Rboretum::getTreeClades(x)})
+    names(tree_clades) <- tree_names
+    tree_count <- length(tree)
+  }
+  
+  if(is.data.frame(test_object)){
+    if(tree_count != 1){ # Single dataframe support must be for one tree
+      return(FALSE)
+    } else if(has_error(silent=TRUE,expr=!all(names(test_object)[1:3] == c("Clade","Mirror_Clade","Split_Node")))){
+      return(FALSE)
+    } else if(!all(names(test_object)[1:3] == c("Clade","Mirror_Clade","Split_Node"))){ # Test that cols 1:3 are split cols
+      return(FALSE)
+    } else if(nrow(test_object) == length(tree_clades)){ # Ensure data
+      return(FALSE)
+    } else if(any(is.na(test_object$Split_Node))){ # Tree support lacks root information
+      return(FALSE)
+    } else if(has_error(silent = TRUE,expr = !all(sort(tree_clades)==sort(as.character(test_object$Clade))))){
+      return(FALSE)
+    } else if(!all(sort(tree_clades)==sort(as.character(test_object$Clade)))){ # Ensure clades match between tree and support
+      return(FALSE)
+    } else{
+      return(TRUE)
+    }
+  }
+  
+  if(is.list(test_object)){
+    
+    # Ensure results list is the same length as trees
+    if(length(test_object) != tree_count){ 
+      return(FALSE)
+    } 
+    
+    # Ensure support list has the same name as trees
+    if(!all(names(test_object) == tree_names)){ 
+      return(FALSE)
+    }
+      
+    # Test that cols 1:3 are split cols
+    err_name <- purrr::map(.x=names(test_object),.f=function(x){has_error(silent=TRUE,expr=all(names(test_object[[x]])[1:3] == c("Clade","Mirror_Clade","Split_Node")))}) %>% unlist()
+    if(any(err_name)){
+      return(FALSE)
+    } 
+    
+    name_check <- purrr::map(.x=names(test_object),.f=function(x){all(names(test_object[[x]])[1:3] == c("Clade","Mirror_Clade","Split_Node"))}) %>% unlist()
+    if(!all(name_check)){
+      return(FALSE)
+    }
+    
+    # Ensure data
+    data_check <- purrr::map(.x=names(test_object),.f=function(x){nrow(test_object[[x]]) == length(Rboretum::getTreeClades(tree[[x]]))}) %>% unlist()
+    if(!all(data_check)){
+      return(FALSE)
+    }
+    
+    # Tree support lacks root information
+    root_check <- purrr::map(.x=names(test_object),.f=function(x){any(is.na(test_object[[x]]$Split_Node))}) %>% unlist()
+    if(any(root_check)){
+      return(FALSE)
+    }
+    
+    # Ensure clades match between tree and support
+    err_clade <- purrr::map(.x=names(test_object),.f=function(x){has_error(silent=TRUE,expr=all(sort(test_object[[x]]$Clade) == Rboretum::getTreeClades(tree[[x]])))}) %>% unlist()
+    if(any(err_name)){
+      return(FALSE)
+    } 
+    
+    clade_check <- purrr::map(.x=names(test_object),.f=function(x){all(sort(test_object[[x]]$Clade) == Rboretum::getTreeClades(tree[[x]]))}) %>% unlist()
+    if(!all(name_check)){
+      return(FALSE)
+    } else{
+      return(TRUE)
+    }
+  }
+}

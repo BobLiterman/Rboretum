d148af53bc9e3682ccaea5ac845870d2c9faf49b
Rachel Schwartz <rsschwartz@gmail.com>
2020-01-29 17:19:04 -0500

    add draft vignette; adjusted tutorial to match vignette

--- inst/Rboretum_Tutorial.R
+++ inst/Rboretum_Tutorial.R
@@ -2,19 +2,11 @@
 # Example Data: 5 genes were sequenced for 20 species. Maxmimum likelihood trees were inferred from the alignments, and results are analyzed here.
 
 library(Rboretum)
-
-# In order to use alignment features, the user must source the scripts
-source_python(system.file("", "Split_Processor.py", package = "Rboretum"))
+#source("R/getTreeClades.R")
 
 # Rboretum can read trees in as rooted phylo objects if the outgroup can be specified
 Gene1_file <- system.file("extdata", "Gene_1.nwk", package = "Rboretum")
 geneTree1 <- readRooted(Gene1_file,root_taxa = c('Species_C','Species_H'))
-
-# Extract monophyletic clades from trees
-getTreeClades(geneTree1)
-
-# Get tree spilts
-getTreeSplits(geneTree1)
 
 # We can quickly plot the tree using treePlotter
 treePlotter(geneTree1)
@@ -31,6 +23,29 @@ treePlotter(geneTree1,xmax = 7,to_color = c('Species_A','Species_B'),colors = "b
 cladesOfInterest = list('Group 1' = c('Species_A','Species_F'),'Group 2'=c('Species_B','Species_O')) # Create a list of taxa to highlight
 treePlotter(geneTree1,xmax = 7,to_color = cladesOfInterest) # Highlight taxa by group
 treePlotter(geneTree1,xmax = 7,to_color = cladesOfInterest,highlight_legend = TRUE,color_branches = TRUE) # Highlight taxa by group
+
+# Trim a tree to a set of taxa
+trimmedTree <- treeTrimmer(geneTree1,taxa = c('Species_A','Species_B','Species_C'))
+treePlotter(trimmedTree)
+
+noSpeciesETree <- treeTrimmer(geneTree1,taxa = 'Species_E',remove = TRUE) # Remove taxa rather than retain
+treePlotter(noSpeciesETree)
+
+# Check if tips are in a tree
+checkTips(trimmedTree,'Species_A')
+checkTips(trimmedTree,'Species_F')
+
+# Extract monophyletic clades from trees
+getTreeClades(geneTree1)
+
+# Get tree spilts
+getTreeSplits(geneTree1)
+
+# Convert labels on tree
+name_df <- read_tsv(system.file("extdata", 'Name_Conversion_Table.tsv', package = "Rboretum"))
+head(name_df)
+renamed_tree <- convertLabels(geneTree1,name_df)
+treePlotter(renamed_tree,xmax = 7,node_label_nudge = 0.2)
 
 # If (1) there are multiple trees to read in, and (2) all trees share the same outgroup, they can be read in together in a few different ways
 file_names <- c('Gene_1.nwk','Gene_2.nwk','Gene_3.nwk','Gene_4.nwk','Gene_5.nwk')
@@ -49,26 +64,13 @@ getTreeClades(allTrees,print_counts = TRUE)
 # Get unique topologies from a multiPhylo
 uniqueTrees <- getUniqueTopologies(allTrees,print_table = TRUE)
 
-# Trim a tree to a set of taxa
-trimmedTree <- treeTrimmer(geneTree1,taxa = c('Species_A','Species_B','Species_C'))
-treePlotter(trimmedTree)
-
-noSpeciesETree <- treeTrimmer(geneTree1,taxa = 'Species_E',remove = TRUE) # Remove taxa rather than retain
-treePlotter(noSpeciesETree)
-
-# Check if tips are in a tree
-checkTips(trimmedTree,'Species_A')
-checkTips(trimmedTree,'Species_F')
-
 checkTips(allTrees,c('Species_A','Species_F')) # Check all trees in a multiPhylo
 checkTips(allTrees,c('Species_A','Species_F'),check_mono = TRUE) # Also check if species are monophyletic
 checkTips(allTrees,c('Species_A','Species_F'),check_mono = TRUE,check_root = TRUE) # Check if species are monophyletic and root
 checkTips(allTrees,c('Species_C','Species_H'),check_mono = TRUE,check_root = TRUE) # Check if species are monophyletic and root
 
-# Convert labels on tree
-name_df <- read_tsv('Name_Conversion_Table.tsv')
-renamed_tree <- convertLabels(geneTree1,name_df)
-treePlotter(renamed_tree,xmax = 7,node_label_nudge = 0.2)
+# In order to use alignment features, the user must source the scripts
+source_python(system.file("", "Split_Processor.py", package = "Rboretum"))
 
 # Reading in mulitple sequence alignments to parse signal
 alignmentSignal <- getAlignmentSignal(data_dir,suffix = 'phylip',use_gaps = FALSE,species_info = allTrees) # Read in alignments from <dir> that end with 'phylip', and process singal for all taxa in 'species_info'

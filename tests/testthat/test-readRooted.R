07b16dffeb45b4f92f3f1e685548087ad7a5be9f
Rachel Schwartz <rsschwartz@gmail.com>
2020-03-17 15:17:21 -0400

    minor test script edits

--- tests/testthat/test-readRooted.R
+++ tests/testthat/test-readRooted.R
@@ -1,17 +1,15 @@
 context("readRooted")
 
 #read tree
+tree <- readRooted("inst/extdata/Gene_1.nwk",root_taxa = c('Species_C','Species_H'))
 
-test_that("test multiphylo", {
-  
-  expect_s3_class(tree, "multiPhylo")
-  
+test_that("reads phylo", {
+  expect_s3_class(tree, "phylo")
 })
 
 test_that("test single root", {
   
-  
-  
+
 })
 
 test_that("multi root", {
@@ -22,4 +20,12 @@ test_that("multi root", {
 
 test_that("warning when there is no data", {
   expect_warning(, "  ")
+})
+
+#read multiphylo
+file_names <- c('inst/extdata/Gene_1.nwk','inst/extdata/Gene_2.nwk','inst/extdata/Gene_3.nwk')
+allTrees <- readRooted(to_root = file_names,root_taxa = c('Species_C','Species_H'))
+
+test_that("reads multiphylo", {
+  expect_s3_class(allTrees, "multiPhylo")
 })

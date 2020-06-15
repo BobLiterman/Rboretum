1ba4d02205f7453b1a2bfc7295f2078f057ab1e1
BobLiterman <jazz710@gmail.com>
2020-06-15 10:12:59 -0400

    Expand semiSorter to accept character vectors

--- R/semiSorter.R
+++ R/semiSorter.R
@@ -1,7 +1,7 @@
 #' Rboretum Semicolon Sorter
 #'
-#' This function returns a sorted, semicolon-delimited string from an unsorted semicolon-delimited string
-#' @param string_to_split Unsorted semicolon delimited string
+#' This function returns a sorted, semicolon-delimited string from a character vector or an unsorted semicolon-delimited string
+#' @param string_to_split Character vector or unsorted semicolon delimited string
 #' @return Sorted semicolon delimited string
 #' @examples 
 #' myShuffledString <- 'c;d;a;b'
@@ -10,5 +10,16 @@
 #' @export
 
 semiSorter <- function(string_to_sort){
-    return(paste(sort(semiVector(string_to_sort)),collapse = ";"))
+  
+  if(!is.character(string_to_sort)){
+    stop("semiSorter expects a character argument")
+  }
+  
+  if(length(string_to_sort)==1){ # Return single elements
+    return(string_to_sort)
+  } else{ # Return sorted + joined character
+    return(paste(naturalsort(string_to_sort),collapse = ";"))
+  } else if(str_detect(string_to_sort,";")){ # If already semicolon-separated, return sorted character vector
+    return(paste(naturalsort(semiVector(string_to_sort)),collapse = ";"))
+  } 
 }

4e396ee0ef9ed012eb00771f655a206d0e82b789
BobLiterman <jazz710@gmail.com>
2020-06-22 11:42:12 -0400

    Update semiVector

--- R/semiVector.R
+++ R/semiVector.R
@@ -1,12 +1,18 @@
 #' Rboretum Semicolon Vectorizer
 #'
 #' This function takes a string separated by semicolons (;) and returns a character vector
-#' @param string_to_split Semicolon delimited character vector of length 1
-#' @return Character vector
+#' @param string_to_split Semicolon delimited character
+#' @return If string_to_split is a single item, semiVector returns a character vector split by ';'. If string_to_split contains multiple items, function returns a list of separated vectors
 #' @examples
 #' myString <- 'a;b;c;d'
 #' semiVector(myString)
-#' > ['a','b','c','d']
+#' > ["a","b","c","d"]
+#' myStrings <- c('a;b;c;d','e;f;g;h)
+#' semiVector(myStrings)
+#' > [[1]]
+#' > [1] ["a","b","c","d"]
+#' > [[2]]
+#' > [1] ["e","f","g","h"]
 #' @export
 
 semiVector <- function(string_to_split){
@@ -14,14 +20,12 @@ semiVector <- function(string_to_split){
   # Ensure character vector of length 1
   if(!is.character(string_to_split)){
     stop("semiVector requires a character argument")
-  } else if(length(string_to_split)!=1){
-    stop("semiVector requires a character argument with a single element")
   }
   
-  # If no ';', return string
-  if(!stringr::str_detect(string_to_split[1],";")){
-    return(string_to_split)
-  } else{ # Return vector split by ;
-    return(stringr::str_split(string_to_split[1],pattern = ';') %>% unlist())
+  if(length(string_to_split)==1){
+      return(stringr::str_split(string_to_split[1],pattern = ';') %>% unlist())
+  } else{
+    sep_list = purrr::map(.x=string_to_split,.f=function(x){stringr::str_split(string_to_split[x],pattern = ';')[[1]]})
+    return(sep_list)
   }
 }

170548bc5fe2690ebe4831a32ed3d365ecdd3a8c
BobLiterman <jazz710@gmail.com>
2020-06-24 21:27:33 -0400

    Add timePlotter and time_lm

---
+++ R/timePlotter.R
@@ -0,0 +1,191 @@
+#' Rboretum Time-Dependent Signal Plotter
+#'
+#' This function takes information about node ages and node support, and plots relative changes in node support over time among datasets. 
+#' @param node_age_df 
+#' @param tree_support 
+#' @param all_sites_col 
+#' @param lm_alpha 
+#' @param wrap
+#' @param wrap_scales
+#' @param return_stats
+#' @return 
+#' @export
+#' 
+timePlotter <- function(node_age_df,tree_support,all_sites_col,lm_alpha,wrap,wrap_scales,return_stats){
+  
+  # Check node age df (Should have 2 columns with prescribed names)
+  if(missing(node_age_df)){
+    stop("'node_age_df' [output from extractNodeAges() or extractNodeAges(return_summary=mean/median)] is required")
+  } else if(!is.data.frame(node_age_df)){
+    stop("'node_age_df' should be the output from extractNodeAges() or extractNodeAges(return_summary=mean/median)")
+  } else if(!ncol(node_age_df) == 2){
+    stop("'node_age_df' should be the output from extractNodeAges() or extractNodeAges(return_summary=mean/median)")
+  } else if(!all(names(node_age_df)==c('Clade','Node_Age'))){
+    stop("'node_age_df' should be the output from extractNodeAges() or extractNodeAges(return_summary=mean/median)")
+  }
+  
+  # Ensure tree_support contains at least two columns of data
+  if(missing(tree_support)){
+    stop("'tree_support' (with 2+ data columns) is required.")
+  } else if(!ncol(tree_support) > 2){
+    stop("'tree_support' needs to contain 2+ columns of support data for timePlotter().")    
+  } else{
+    
+    # Ensure node_age_df contains information about all splits in tree_support
+    node_age_clades <- node_age_df$Clade
+    
+    if(!Rboretum::isTreeSupport(test_object = tree_support,test_clade = node_age_clades,partial = TRUE)){
+      stop("Clades from 'node_age_df' are missing from 'tree_support")
+    } else{
+      
+      tree_support <- tree_support %>% filter(Clade %in% node_age_clades)
+      tree_support_clades <- tree_support %>% pull(Clade)
+      
+      # Check for an 'All Sites' column
+      if(missing(all_sites_col)){
+        tree_support <- tree_support
+        all_sites_filter <- FALSE
+      } else if(!is.character(all_sites_col)){
+        stop("'all_sites_col' should be a character identifying a column in the 'tree_support' dataframe")
+      } else if(length(all_sites_col)!=1){
+        stop("'all_sites_col' should be a character identifying a column in the 'tree_support' dataframe")
+      } else if(!all_sites_col %in% names(tree_support)){
+        stop("'all_sites_col' should be a character identifying a column in the 'tree_support' dataframe")
+      } else{
+        all_sites_support <- tree_support %>% pull(all_sites_col)
+        tree_support <- tree_support %>% select(-all_sites_col)
+        all_sites_filter <- TRUE
+      }
+      
+      support_col_nums <- 2:ncol(tree_support)
+      support_col_names <- names(tree_support)[support_col_nums]
+      support_count <- length(support_col_nums)
+      dataset_colors <- viridisLite::viridis(support_count)
+      
+      node_age_vector <- node_age_df$Node_Age
+      names(node_age_vector) <- node_age_df$Clade
+      tree_support_ages <- purrr::map(.x=tree_support_clades,.f=function(x){node_age_vector[x]}) %>% unlist()
+    }
+  }
+  
+  # Get wrap/scale information
+  if(missing(wrap)){
+    wrap <- FALSE
+    wrap_scales <- NULL
+  } else if(!is.logical(wrap)){
+    warning("'wrap' should be TRUE or FALSE. Plotting without facet wrapping...")
+    wrap <- FALSE
+  } else if(wrap){
+    if(missing(wrap_scales)){
+      wrap_scales <- 'free'
+    } else if(!is.character(wrap_scales)){
+      wrap_scales <- 'free'
+      warning("'wrap_scales' should be 'free' [default] or 'fixed'. Plotting with free scales...")
+    } else if(length(wrap_scales) > 1){
+      wrap_scales <- 'free'
+      warning("'wrap_scales' should be 'free' [default] or 'fixed'. Plotting with free scales...")
+    } else if(!(wrap_scales %in% c('free','fixed'))){
+      wrap_scales <- 'free'
+      warning("'wrap_scales' should be 'free' [default] or 'fixed'. Plotting with free scales...")
+    }
+  }
+  
+  plot_df <- tibble(Clade=character(),Node_Age=numeric(),Dataset=character(),Total_Support=numeric(),Percent_Support=numeric(),Plot_Color=character())
+  
+  if(!all_sites_filter){
+    # 'Percent_Support' = Dataset support / Sum of Node support across datasets
+    
+    # Get total support for each clade across datasets
+    total_clade_support <- rowSums(tree_support[support_col_nums])
+    
+    for(i in 1:support_count){
+      dataset_name <- support_col_names[i]
+      dataset_counts <- pull(tree_support,support_col_nums[i])
+      dataset_percents = 100*(dataset_counts/total_clade_support)
+      temp_df <- tibble(Clade=tree_support_clades,Node_Age=tree_support_ages,Dataset=dataset_name,Total_Support=total_clade_support,Percent_Support=dataset_percents,Plot_Color=dataset_colors[[i]])
+      plot_df <- rbind(plot_df,temp_df)
+    }
+  } else{
+    # 'Percent_Support' = Dataset support / Support from 'all sites' 
+    
+    for(i in 1:support_count){
+      dataset_name <- support_col_names[i]
+      dataset_counts <- pull(tree_support,support_col_nums[i])
+      dataset_percents = 100*(dataset_counts/all_sites_support)
+      temp_df <- tibble(Clade=tree_support_clades,Node_Age=tree_support_ages,Dataset=dataset_name,Total_Support=all_sites_support,Percent_Support=dataset_percents,Plot_Color=dataset_colors[[i]])
+      plot_df <- rbind(plot_df,temp_df)
+    }
+  }
+  
+  # Run linear models if 'lm_alpha' arguement is passed
+  if(missing(lm_alpha)){
+    run_lm <- FALSE
+  } else if(!is.numeric(lm_alpha)){
+    warning("'lm_alpha' should be a critical p-value threshold...performing statstical assessment at default alpha of 0.05...")
+    lm_alpha <- 0.05
+    run_lm <- TRUE
+  } else if(lm_alpha >= 1 | lm_alpha <= 0){
+    warning("'lm_alpha' should be a critical p-value threshold between 0 and 1...performing statstical assessment at default alpha of 0.05...")
+    lm_alpha <- 0.05
+    run_lm <- TRUE
+  } else{
+    run_lm <- TRUE
+  }
+  
+  if(run_lm){
+    lm_df <- time_lm(plot_df %>% select(Node_Age,Dataset,Percent_Support),lm_alpha)
+    
+    plot_df <- plot_df %>% 
+      left_join(lm_df,by='Dataset') %>%
+      mutate(Slope = 100*Slope) %>%
+      mutate(Plot_Fill = ifelse(BF_Sig =="N","white",Plot_Color)) %>%
+      mutate(Plot_Shape = ifelse(BF_Sig =="N",1,23))
+  } else{
+    plot_df <- plot_df %>% mutate(Plot_Fill = Plot_Color) %>%
+      mutate(Plot_Shape = 21)
+  }
+  
+  # Pull out color information for legend
+  color_df <- plot_df[!duplicated(plot_df$Dataset),] %>% select(Dataset,Plot_Color)
+  plot_colors <- pull(color_df,Plot_Color)
+  names(plot_colors) <- pull(color_df,Dataset)
+  
+  base_plot <- ggplot(plot_df,aes(x=Node_Age,y=Percent_Support,color=Dataset)) +
+    geom_jitter(size=4,aes(fill=Plot_Fill,shape=Plot_Shape)) +
+    geom_smooth(size=1.5,method=lm,se=FALSE,show.legend = FALSE,aes(fill=Plot_Fill)) +
+    scale_fill_identity() +
+    scale_color_manual(values = plot_colors,guide="legend") +
+    scale_shape_identity() +
+    theme_bw() +
+    xlab("\nTime Since Split\n") +
+    ylab("Proportion of Phylogenetic Signal (%)\n") +
+    scale_x_reverse() +
+    theme(axis.title=element_text(size=14,face="bold"),
+          axis.text.x=element_text(size=13),
+          axis.text.y = element_text(size=13,face="bold"))
+  
+  if(!wrap){ # One plot
+    return_plot <- base_plot +
+      guides(shape = guide_legend(override.aes = list(size = 4))) +
+      theme(legend.title = element_text(hjust = 0.5, size=14, face="bold"),
+            legend.text = element_text(size=12))
+    
+  } else{ # Wrapped plot
+    return_plot <- base_plot +
+      facet_wrap(~Dataset,scales=wrap_scales) +
+      theme(panel.spacing = unit(1.5, "lines"),
+            legend.position = "none",
+            strip.text.x = element_text(size = 12,face="bold")) 
+  }
+  
+  if(missing(return_stats)){
+    return(return_plot)
+  } else if(!is.logical(return_stats)){
+    warning("'return_stats' should be TRUE or FALSE. Not returning stats table...")
+    return(return_plot)
+  } else if(run_lm & return_stats){
+    return(list(Plot=return_plot,Stats=lm_df))
+  } else{
+    return(return_plot)
+  }
+}
---
+++ R/time_lm.R
@@ -0,0 +1,47 @@
+#' Rboretum Time Linear Modeler
+#'
+#' This function takes information about node ages and node support, and uses linear models and Bonferroni correction to assess changes in relative phylogenetic utility over time among datasets
+#' @param age_support_df 
+#' @param lm_alpha 
+#' @return 
+#' @export
+#' 
+time_lm <- function(age_support_df,lm_alpha){
+  
+  # Check age_support_df
+  if(!is.data.frame(age_support_df)){
+    stop("'age_support_df' [usually passed from timePlotter()] does not appear to be a dataframe.")
+  } else if(ncol(age_support_df)!=3){
+    stop("'age_support_df' [usually passed from timePlotter()] does not contain 3 columns.")
+  } else if(!all(names(age_support_df)==c('Node_Age','Dataset','Percent_Support'))){
+    stop("'age_support_df' [usually passed from timePlotter()] does not contain columns named ['Node_Age','Dataset','Percent_Support'] .")
+  }
+  
+  if(missing(lm_alpha)){
+    lm_alpha <- 0.05
+  }
+  
+  # Get dataset counts and set signficance based on Bonferroni
+  datasets <- unique(age_support_df$Dataset)
+  critical_value <- lm_alpha/length(datasets)
+  
+  stat_list <- c()
+  r_list  <- c()
+  intercept_list <- c()
+  
+  for(dataset in datasets){
+    
+    focal_lm <- age_support_df %>% filter(Dataset==dataset) %>% lm(Percent_Support ~ Node_Age, data=.)
+    
+    stat_list[[length(stat_list)+1]] <-t(data.frame(summary(focal_lm)$coefficients[2,]))
+    r_list[[length(r_list)+1]] <-summary(focal_lm)$adj.r.squared
+    intercept_list[[length(intercept_list)+1]] <-focal_lm$coefficients[[1]]
+  }
+  
+  return(data.frame(matrix(unlist(stat_list), nrow=length(stat_list), byrow=T)) %>% 
+           rename(Slope='X1',StdErr='X2',t_value='X3',p_value = 'X4') %>% 
+           mutate(Dataset=datasets) %>%
+           mutate(BF_Sig = ifelse(p_value <= critical_value,"Y","N")) %>%
+           mutate(Adj_R_Sq=r_list) %>%
+           mutate(Intercept=intercept_list))
+}

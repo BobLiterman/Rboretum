91d8794611331f107c7e729c971f5b9e7a82ac3c
BobLiterman <jazz710@gmail.com>
2020-07-09 14:26:50 -0400

    Align tip labels

--- R/treePlotter.R
+++ R/treePlotter.R
@@ -731,17 +731,17 @@ treePlotter <- function(tree,basic_plot,tree_support,clade_support,geom_size,sca
     
     # Process tip labels
     if(!colorTips | color_branches){
-      return_tree <- return_tree + geom_tiplab(size=taxa_font_size,fontface=taxa_fontface,offset = taxa_offset)
+      return_tree <- return_tree + geom_tiplab(size=taxa_font_size,fontface=taxa_fontface,offset = taxa_offset,align=TRUE,linetype=NA)
     } else{
       if(is.character(to_color)){
-        return_tree <- return_tree + geom_tiplab(size=taxa_font_size,fontface=taxa_fontface,offset = taxa_offset,aes(color=group),show.legend=FALSE) +
+        return_tree <- return_tree + geom_tiplab(size=taxa_font_size,fontface=taxa_fontface,offset = taxa_offset,aes(color=group),show.legend=FALSE,align=TRUE,linetype=NA) +
           scale_color_manual(values = colors)
       } else if(is.list(to_color)){
         if(highlight_legend){
-          return_tree <- return_tree + geom_tiplab(size=taxa_font_size,fontface=taxa_fontface,offset = taxa_offset,aes(color=group)) +
+          return_tree <- return_tree + geom_tiplab(size=taxa_font_size,fontface=taxa_fontface,offset = taxa_offset,aes(color=group),align=TRUE,linetype=NA) +
             scale_color_manual("Focal Clades",breaks = names(to_color),values = colors)          
         } else{
-          return_tree <- return_tree + geom_tiplab(size=taxa_font_size,fontface=taxa_fontface,offset = taxa_offset,aes(color=group),show.legend=FALSE) +
+          return_tree <- return_tree + geom_tiplab(size=taxa_font_size,fontface=taxa_fontface,offset = taxa_offset,aes(color=group),show.legend=FALSE,align=TRUE,linetype=NA) +
             scale_color_manual("Focal Clades",breaks = names(to_color),values = colors)
         }
       }

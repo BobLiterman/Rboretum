311ce7c982fa98de804f95e7d9859ea772f2fcee
BobLiterman <jazz710@gmail.com>
2020-07-13 14:15:14 -0400

    Update test for convertLabels

--- tests/testthat/test-checkTips.R
+++ tests/testthat/test-checkTips.R
@@ -6,14 +6,14 @@ test_tree <- Rboretum::readRooted("inst/extdata/unrootedTrees/Gene_1.nwk",root_t
 # Remove tip 'Species_A'
 trimmed_tree <- Rboretum::treeTrimmer(test_tree,taxa = 'Species_A',remove=TRUE)
 
-test_that("taxa present as expected", {
+test_that("Expected taxa present in tree...", {
   expect_true(checkTips(test_tree,'Species_A'))
   expect_true(checkTips(test_tree,c('Species_C','Species_H'),check_root=TRUE))
   expect_false(checkTips(test_tree,'Species_Q')) # Species not present in tree
   expect_false(checkTips(trimmed_tree,'Species_A'))
 })
 
-# Read in  MultiPhylo
+# Read in multiPhylo
 file_names <- c('inst/extdata/unrootedTrees/Gene_1.nwk','inst/extdata/unrootedTrees/Gene_2.nwk','inst/extdata/unrootedTrees/Gene_3.nwk')
 three_trees <- readRooted(to_root = file_names,root_taxa = c('Species_C','Species_H'))
 
@@ -21,7 +21,7 @@ three_trees <- readRooted(to_root = file_names,root_taxa = c('Species_C','Specie
 three_trees_b <- three_trees
 three_trees_b[[1]] <- trimmed_tree
 
-test_that("taxa present as expected multiphylo", {
+test_that("Expected taxa present in trees...", {
   expect_true(checkTips(three_trees,'Species_A'))
   expect_false(checkTips(three_trees,'Species_Q')) # Species not present in tree
   expect_true(checkTips(three_trees,c('Species_C','Species_H'),check_root=TRUE))
--- tests/testthat/test-convertLabels.R
+++ tests/testthat/test-convertLabels.R
@@ -1,26 +1,35 @@
 context("convertLabels")
 
-#read phylo
+# Read in test tree and name conversion table
+test_tree <- Rboretum::readRooted("inst/extdata/unrootedTrees/Gene_1.nwk",root_taxa = c('Species_C','Species_H'))
+name_table <- read_tsv('inst/extdata/Name_Conversion_Table.tsv')
 
-#run
+# Convert labels
+renamed_tree <- Rboretum::convertLabels(to_convert = test_tree,name_df = name_table,from = 'Alignment_IDs',to='Common_Names')
 
-test_that("test multiphylo", {
-  
-  expect_s3_class(tree, "phylo")
-  
+test_that("Renamed tree is a phylo...", {
+  expect_s3_class(renamed_tree, "phylo")
 })
 
-test_that("names substituted correctly", {
-  
-
-  
+test_that("Phylo names substituted correctly...", {
+  expect_true(checkTips(test_tree,'Species_A'))
+  expect_false(checkTips(renamed_tree,'Species_A'))
+  expect_true(checkTips(renamed_tree,'Fuzzy Wollop'))
+  expect_false(checkTips(test_tree,'Fuzzy Wollop'))
 })
 
+# Read in multiPhylo
+file_names <- c('inst/extdata/unrootedTrees/Gene_1.nwk','inst/extdata/unrootedTrees/Gene_2.nwk','inst/extdata/unrootedTrees/Gene_3.nwk')
+three_trees <- readRooted(to_root = file_names,root_taxa = c('Species_C','Species_H'))
+renamed_trees <- Rboretum::convertLabels(to_convert = three_trees,name_df = name_table,from = 'Alignment_IDs',to='Common_Names')
 
-#repeat for multiphylo
+test_that("Renamed tree is a multiPhylo...", {
+  expect_s3_class(renamed_tree, "multiPhylo")
+})
 
-test_that("test multiphylo", {
-  
-  expect_s3_class(tree, "multiPhylo")
-  
+test_that("multiPhylo names substituted correctly...", {
+  expect_true(checkTips(three_trees,'Species_A'))
+  expect_false(checkTips(renamed_trees,'Species_A'))
+  expect_true(checkTips(renamed_trees,'Fuzzy Wollop'))
+  expect_false(checkTips(three_trees,'Fuzzy Wollop'))  
 })

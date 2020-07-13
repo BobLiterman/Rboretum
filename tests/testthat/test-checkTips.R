8c0df5262a54572f2d0ecd8a805fed6c7b992533
BobLiterman <jazz710@gmail.com>
2020-07-13 13:43:42 -0400

    Update checkTips test

--- tests/testthat/test-checkTips.R
+++ tests/testthat/test-checkTips.R
@@ -1,23 +1,35 @@
 context("checkTips")
 
-#read tree
-tree2 <- readRooted("inst/extdata/Gene_1.nwk",root_taxa = c('Species_C','Species_H'))
-trimmedTree <- treeTrimmer(tree2,taxa = paste0("Species_", LETTERS[2:14])) #base
+# Read in test tree
+test_tree <- Rboretum::readRooted("inst/extdata/unrootedTrees/Gene_1.nwk",root_taxa = c('Species_C','Species_H'))
+
+# Remove tip 'Species_A'
+trimmed_tree <- Rboretum::treeTrimmer(test_tree,taxa = 'Species_A',remove=TRUE)
 
 test_that("taxa present as expected", {
-  expect_true(checkTips(tree2,'Species_A'))
-  expect_false(checkTips(tree2,'Species_Q'))
-  expect_false(checkTips(trimmedTree,'Species_A'))
+  expect_true(checkTips(test_tree,'Species_A'))
+  expect_true(checkTips(test_tree,'Species_C',check_root=TRUE))
+  expect_true(checkTips(test_tree,'Species_H',check_root=TRUE))
+  expect_true(checkTips(test_tree,c('Species_C','Species_H'),check_root=TRUE))
+  expect_false(checkTips(test_tree,'Species_Q'))
+  expect_false(checkTips(trimmed_tree,'Species_A'))
 })
 
-#read multiphylo
-file_names <- c('inst/extdata/Gene_1.nwk','inst/extdata/Gene_2.nwk','inst/extdata/Gene_3.nwk')
-allTrees <- readRooted(to_root = file_names,root_taxa = c('Species_C','Species_H'))
-allTrees_difspp <- allTrees
-allTrees_difspp[[1]] <- trimmedTree
+# Read in  MultiPhylo
+file_names <- c('inst/extdata/unrootedTrees/Gene_1.nwk','inst/extdata/unrootedTrees/Gene_2.nwk','inst/extdata/unrootedTrees/Gene_3.nwk')
+three_trees <- readRooted(to_root = file_names,root_taxa = c('Species_C','Species_H'))
+
+# Set tree 1 to trimmed tree without 'Species_A'
+three_trees_b <- three_trees
+three_trees_b[[1]] <- trimmed_tree
 
 test_that("taxa present as expected multiphylo", {
-  expect_true(checkTips(allTrees,'Species_A'))
-  expect_false(checkTips(allTrees,'Species_Q'))
-  expect_false(checkTips(allTrees_difspp,'Species_A')) #one tree is missing the species
+  expect_true(checkTips(three_trees,'Species_A'))
+  expect_false(checkTips(three_trees,'Species_Q'))
+  expect_true(checkTips(three_trees,'Species_C',check_root=TRUE))
+  expect_true(checkTips(three_trees,'Species_H',check_root=TRUE))
+  expect_true(checkTips(three_trees,c('Species_C','Species_H'),check_root=TRUE))
+  expect_false(checkTips(three_trees_b,'Species_A')) #one tree is missing the species
+  expect_true(checkTips(three_trees_b,'Species_C',check_root=TRUE))
+  expect_true(checkTips(three_trees_b,'Species_H',check_root=TRUE))
 })

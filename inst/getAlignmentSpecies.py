f409638d27fd807021007327333a07763d070ab8
BobLiterman <jazz710@gmail.com>
2020-07-23 09:46:27 -0400

    Add getAlignmentSpecies.py

---
+++ inst/getAlignmentSpecies.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+    This script will take an alignment file (NEXUS, FASTA, or Phylip-relaxed) and return the sequence names
+"""
+from Bio import AlignIO, SeqIO
+
+def getAlignmentSpecies(alignment_path):
+    
+    formats = {'nex': 'nexus', 'nexus': 'nexus',
+               'phy': 'phylip', 'phylip-relaxed': 'phylip-relaxed', 'phylip': 'phylip',
+               'fa': 'fasta', 'fasta': 'fasta'}
+
+    fformat = formats[alignment_path.split('.')[-1]]
+    
+    try:
+        align = AlignIO.read(alignment_path, fformat)
+
+        # Get alignment species
+        align_species = list()
+        for seq_record in align:
+            align_species.append(seq_record.id)
+
+        return align_species
+    
+    except:
+        return []

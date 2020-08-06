2d6532c3f95f2ad8dbae7eee3bb0813ee9a62445
BobLiterman <jazz710@gmail.com>
2020-08-06 13:09:51 -0400

    Source python globally

--- R/sourceRboretum.R
+++ R/sourceRboretum.R
@@ -4,9 +4,9 @@
 #' @export
 #' 
 sourceRboretum <- function(){
-  source_python(system.file("", "Alignment_Species.py", package = "Rboretum"))
-  source_python(system.file("", "Splits_Processor.py", package = "Rboretum"))
-  source_python(system.file("", "Alignment_Patterns.py", package = "Rboretum"))
-  source_python(system.file("", "Alignment_Composition.py", package = "Rboretum"))
-  source_python(system.file("", "Species_Composition.py", package = "Rboretum"))
+  source_python(system.file("", "Alignment_Species.py", package = "Rboretum"),envir=globalenv())
+  source_python(system.file("", "Splits_Processor.py", package = "Rboretum"),envir=globalenv())
+  source_python(system.file("", "Alignment_Patterns.py", package = "Rboretum"),envir=globalenv())
+  source_python(system.file("", "Alignment_Composition.py", package = "Rboretum"),envir=globalenv())
+  source_python(system.file("", "Species_Composition.py", package = "Rboretum"),envir=globalenv())
 }

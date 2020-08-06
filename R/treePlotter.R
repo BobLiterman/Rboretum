f616cc4fb9fae28aff979eaabb40b0027385245a
BobLiterman <jazz710@gmail.com>
2020-08-06 14:52:50 -0400

    Negate taxa_offset if reverse_x = TRUE

--- R/treePlotter.R
+++ R/treePlotter.R
@@ -732,14 +732,14 @@ treePlotter <- function(tree,basic_plot,tree_support,clade_support,geom_size,sca
     # Process tip labels
     if(!colorTips | color_branches){
       if(reverse_x){
-        return_tree <- return_tree + geom_tiplab(size=taxa_font_size,fontface=taxa_fontface,offset = taxa_offset,align=TRUE,linetype=NA,hjust=1)
+        return_tree <- return_tree + geom_tiplab(size=taxa_font_size,fontface=taxa_fontface,offset = -taxa_offset,align=TRUE,linetype=NA,hjust=1)
       } else{
         return_tree <- return_tree + geom_tiplab(size=taxa_font_size,fontface=taxa_fontface,offset = taxa_offset)
       }
     } else{
       if(is.character(to_color)){
         if(reverse_x){
-          return_tree <- return_tree + geom_tiplab(size=taxa_font_size,fontface=taxa_fontface,offset = taxa_offset,aes(color=group),show.legend=FALSE,align=TRUE,linetype=NA,hjust=1) +
+          return_tree <- return_tree + geom_tiplab(size=taxa_font_size,fontface=taxa_fontface,offset = -taxa_offset,aes(color=group),show.legend=FALSE,align=TRUE,linetype=NA,hjust=1) +
             scale_color_manual(values = colors)
         } else{
           return_tree <- return_tree + geom_tiplab(size=taxa_font_size,fontface=taxa_fontface,offset = taxa_offset,aes(color=group),show.legend=FALSE) +
@@ -749,7 +749,7 @@ treePlotter <- function(tree,basic_plot,tree_support,clade_support,geom_size,sca
       } else if(is.list(to_color)){
         if(highlight_legend){
           if(reverse_x){
-            return_tree <- return_tree + geom_tiplab(size=taxa_font_size,fontface=taxa_fontface,offset = taxa_offset,aes(color=group),align=TRUE,linetype=NA,hjust=1) +
+            return_tree <- return_tree + geom_tiplab(size=taxa_font_size,fontface=taxa_fontface,offset = -taxa_offset,aes(color=group),align=TRUE,linetype=NA,hjust=1) +
               scale_color_manual("Focal Clades",breaks = names(to_color),values = colors)               
           } else{
             return_tree <- return_tree + geom_tiplab(size=taxa_font_size,fontface=taxa_fontface,offset = taxa_offset,aes(color=group)) +
@@ -757,7 +757,7 @@ treePlotter <- function(tree,basic_plot,tree_support,clade_support,geom_size,sca
           }
         } else{
           if(reverse_x){
-            return_tree <- return_tree + geom_tiplab(size=taxa_font_size,fontface=taxa_fontface,offset = taxa_offset,aes(color=group),show.legend=FALSE,align=TRUE,linetype=NA,hjust=1) +
+            return_tree <- return_tree + geom_tiplab(size=taxa_font_size,fontface=taxa_fontface,offset = -taxa_offset,aes(color=group),show.legend=FALSE,align=TRUE,linetype=NA,hjust=1) +
               scale_color_manual("Focal Clades",breaks = names(to_color),values = colors)
           } else{
             return_tree <- return_tree + geom_tiplab(size=taxa_font_size,fontface=taxa_fontface,offset = taxa_offset,aes(color=group),show.legend=FALSE) +

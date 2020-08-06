72313bb89a7f294248c76f74258ec1d46a2be2cd
BobLiterman <jazz710@gmail.com>
2020-08-06 14:57:14 -0400

    Set test_data_dir with sourceRboretum()

--- R/sourceRboretum.R
+++ R/sourceRboretum.R
@@ -1,12 +1,16 @@
 #' Rboretum Python Script Sourcer
 #'
-#' This function sources all Rborteum Python scripts for 'reticulate'
+#' This function sources all Rborteum Python scripts for 'reticulate', and globally assigns the directory containing the test data to 'test_data_dir'
 #' @export
 #' 
 sourceRboretum <- function(){
+  
+  # Source python scripts
   source_python(system.file("", "Alignment_Species.py", package = "Rboretum"),envir=globalenv())
   source_python(system.file("", "Splits_Processor.py", package = "Rboretum"),envir=globalenv())
   source_python(system.file("", "Alignment_Patterns.py", package = "Rboretum"),envir=globalenv())
   source_python(system.file("", "Alignment_Composition.py", package = "Rboretum"),envir=globalenv())
   source_python(system.file("", "Species_Composition.py", package = "Rboretum"),envir=globalenv())
+  
+  test_data_dir <<- system.file("extdata",package = 'Rboretum')
 }

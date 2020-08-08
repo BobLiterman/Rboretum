70f64429a42eee577fdf532510b0ec1ebb9c2862
BobLiterman <jazz710@gmail.com>
2020-08-08 09:43:53 -0400

    Docs

--- docs/conf.py
+++ docs/conf.py
@@ -74,9 +74,9 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'msmb_theme'
-import msmb_theme
-html_theme_path = [msmb_theme.get_html_theme_path()]
+import rtcat_sphinx_theme
+html_theme = "rtcat_sphinx_theme"
+html_theme_path = [rtcat_sphinx_theme.get_html_theme_path()]
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.

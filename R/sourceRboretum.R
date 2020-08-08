1b31d7a5b3a25d21b9868677e7e1b4b98ea4fcc6
BobLiterman <jazz710@gmail.com>
2020-08-08 13:06:10 -0400

    Add more test data variables

--- R/sourceRboretum.R
+++ R/sourceRboretum.R
@@ -1,6 +1,6 @@
 #' Rboretum Python Script Sourcer
 #'
-#' This function sources all Rborteum Python scripts for 'reticulate', and globally assigns the directory containing the test data to 'rboretum_example_data_dir'
+#' This function sources all Rborteum Python scripts for 'reticulate', and globally assigns the test data directories
 #' @export
 #' 
 sourceRboretum <- function(){
@@ -13,4 +13,30 @@ sourceRboretum <- function(){
   source_python(system.file("", "Species_Composition.py", package = "Rboretum"),envir=globalenv())
   
   rboretum_example_data_dir <<- system.file("extdata",package = 'Rboretum')
+  rb_unroot_dir <<- paste0(rboretum_example_data_dir,'/unrootedTrees')
+  rb_alignment_dir <<- paste0(rboretum_example_data_dir,'/alignments')
+  rb_timeTree_dir <<- paste0(rboretum_example_data_dir,'/timeTrees')
+  
+  rb_tree1_path <<- paste0(rb_unroot_dir,'/Gene_1.nwk')
+  rb_tree2_path <<- paste0(rb_unroot_dir,'/Gene_2.nwk')
+  rb_tree3_path <<- paste0(rb_unroot_dir,'/Gene_3.nwk')
+  rb_tree4_path <<- paste0(rb_unroot_dir,'/Gene_4.nwk')
+  rb_tree5_path <<- paste0(rb_unroot_dir,'/Gene_5.nwk')
+  
+  rb_all_unrooted <<- c(rb_tree1_path,rb_tree2_path,rb_tree3_path,rb_tree4_path,rb_tree5_path)
+  
+  rb_align1_path <<- paste0(rb_unroot_dir,'/Gene_1.phylip')
+  rb_align2_path <<- paste0(rb_unroot_dir,'/Gene_2.phylip')
+  rb_align3_path <<- paste0(rb_unroot_dir,'/Gene_3.phylip')
+  rb_align4_path <<- paste0(rb_unroot_dir,'/Gene_4.phylip')
+  rb_align5_path <<- paste0(rb_unroot_dir,'/Gene_5.phylip')
+  
+  rb_all_align <<- c(rb_align1_path,rb_align2_path,rb_align3_path,rb_align4_path,rb_align5_path)
+  
+  rb_timeTree1_path <<- paste0(rb_timeTree_dir,"/Chronogram_1.nwk")
+  rb_timeTree2_path <<- paste0(rb_timeTree_dir,"/Chronogram_2.nwk")
+  rb_timeTree3_path <<- paste0(rb_timeTree_dir,"/Chronogram_3.nwk")
+  rb_timeTreeM_path <<- paste0(rb_timeTree_dir,"/Chronogram_MultiPhylo.nwk")
+  
+  rb_all_timeTree <<- c(rb_timeTree1_path,rb_timeTree2_path,rb_timeTree3_path)
 }

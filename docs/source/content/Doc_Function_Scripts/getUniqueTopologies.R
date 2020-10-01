c5a717d9a5e7e3e2df1bde078defef903c38b503
Literman <Robert.Literman@fda.gov>
2020-10-01 11:31:47 -0400

    Fixed typos

--- docs/source/content/Doc_Function_Scripts/getUniqueTopologies.R
+++ docs/source/content/Doc_Function_Scripts/getUniqueTopologies.R
@@ -4,14 +4,14 @@ sourceRboretum()
 # Read in multiPhylo 
 myMultiPhylo <- readRooted(to_root = rb_all_unrooted,root_taxa = c('Species_C','Species_H'))
 myMultiPhylo
-treePlotter(myMultiPhylo,basic_plot = TRUE,xmax=10,taxa_font_size = 3,node_label_font_size = 3)
+treePlotter(myMultiPhylo,basic_plot = TRUE,xmin=-0.5,xmax=10,taxa_font_size = 3,node_label_font_size = 3)
 
 # Reduce to unique topologies, use tree names for new trees
 myUniqueTrees_1 <- getUniqueTopologies(trees = myMultiPhylo,tree_names = TRUE,print_table = TRUE)
 myUniqueTrees_1
-treePlotter(myUniqueTrees_1,basic_plot = TRUE,xmax=10,taxa_font_size = 3,node_label_font_size = 3)
+treePlotter(myUniqueTrees_1,basic_plot = TRUE,xmin=-0.5,xmax=10,taxa_font_size = 3,node_label_font_size = 3)
 
-# Reduce to unique topolodies, use dummy names for new trees
+# Reduce to unique topologies, use dummy names for new trees
 myUniqueTrees_2 <- getUniqueTopologies(trees = myMultiPhylo,tree_names = FALSE,print_table = TRUE)
 myUniqueTrees_2
 treePlotter(myUniqueTrees_2,basic_plot = TRUE,xmax=10,taxa_font_size = 3,node_label_font_size = 3)

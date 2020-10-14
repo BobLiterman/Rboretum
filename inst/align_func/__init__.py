062f24cb8a8db9ad9788a4fb84a541fc0458d550
Literman <Robert.Literman@fda.gov>
2020-10-14 12:02:07 -0400

    import garbage

--- inst/align_func/__init__.py
+++ inst/align_func/__init__.py
@@ -3,4 +3,5 @@
 import os
 import sys
 
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+from .Site_Counter import countAs,countCs,countGs,countTs,countNs,countGaps
+from .Alignment_Reader import getPrunedAlignment

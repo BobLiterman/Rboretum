bec619b1c57c6225816c311511db43eeecc3fea8
Literman <Robert.Literman@fda.gov>
2020-10-15 15:07:51 -0400

    test script

--- docs/source/content/Doc_Function_Scripts/getAlignmentComposition2.R
+++ docs/source/content/Doc_Function_Scripts/getAlignmentComposition2.R
@@ -1,10 +1,13 @@
 devtools::install_github('BobLiterman/Rboretum',upgrade=FALSE)
 library(Rboretum)
+sourceRboretum()
 
 # Set path to alignment data
 myManualAlignment <- 'inst/extdata/alignments/Gene_1.phylip'
 mySpecies <- getAlignmentSpecies(myManualAlignment)
 myName <- 'Gene_1'
 
-rb_run_align_comp('inst/',myManualAlignment,mySpecies,myName)
-rb_run_species_comp('inst/',myManualAlignment,mySpecies,myName)
+getAlignmentComposition(myManualAlignment,mySpecies,myName)
+getSpeciesComposition(myManualAlignment,mySpecies,myName)
+getAlignmentSignal(myManualAlignment,mySpecies,TRUE,myName)
+getAlignmentPatterns(myManualAlignment,mySpecies,TRUE,myName)
